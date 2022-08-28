c192f279624415ba3e8f7fb4bf6fb6d2c7e93e23
Omer Cavanshirli <omrcvnsrli@hotmail.com>
2022-08-29 03:52:32 +0400

    new update

---
+++ chatAPP/Human.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace chatAPP
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string Picture { get; set; }
+    }
+}
