fa83a8d0c38fcd2045e2810c550cfe8d190d360e
Omer Cavanshirli <omrcvnsrli@hotmail.com>
2022-08-31 03:58:08 +0400

    new update

--- chatAPP/MainWindow.xaml.cs
+++ chatAPP/MainWindow.xaml.cs
@@ -39,20 +39,66 @@ namespace chatAPP
                new Human
                {
                    Name="Rihanna",
-                   Message ="don't leave me, please"
+                   Message ="don't leave me, please",
+                   Image="Image/rihanna.jpg"
                },
                new Human
                {
                    Name="Ester",
-                   Message="do you have free time tomorrow?"
+                   Message="do you have free time tomorrow?",
+                   Image="Image/ester.jpg"
                },
                new Human
                {
                    Name="Lena del",
-                   Message="yeterday was a very good day for me. Thank you everythink"
+                   Message="yeterday was a very good day for me. Thank you everythink",
+                   Image="Image/lena.jpg"
                }
             };
         }
 
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            if (searchLbl.Content.ToString() == "Search")
+            {
+                searchLbl.Content = string.Empty;
+            }
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (searchLbl.Content.ToString() == string.Empty)
+            {
+                searchLbl.Content = "Search";
+            }
+        }
+
+        private void chatBorder_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var resultBorder = sender as Border;
+            var grid = resultBorder.Child as Grid;
+
+            foreach (var item in grid.Children)
+            {
+                if (item is Grid grid2)
+                {
+                    foreach (var item2 in grid2.Children)
+                    {
+                        if (item2 is Label l)
+                        {
+                            if (l.Name.ToString() == "nameLbl")
+                                nameLbl2.Content = l.Content;
+                            else if (l.Name.ToString() == "messageLbl")
+                                messageLbl2.Content = l.Content;
+                        }
+                    }
+                }
+                else if (item is Ellipse el)
+                {
+                    
+                }
+            }
+        }
     }
 }
